35ee830ea014f7b4d1c0c0fb068fd38f48ed4d1e
Josem-14 <josemiguelsedano9@gmail.com>
2025-10-02 00:28:03 -0500

    Update and rename Punto 1. to Punto 1.cpp

---
+++ Punto 1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;  
+    cout << "Ingrese el tamaño del arreglo: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cout << "Ingrese el número " << i << ": ";
+        cin >> nums[i];
+    }
+
+    int v; 
+    cout << "Ingrese el número objetivo: ";
+    cin >> v;
+
+    int m, j; 
+    int a;    
+
+    for (m = 0; m < n; m++) {
+        for (a = 0; a < n; a++) {
+            if (m != a) {  
+                if (nums[m] + nums[a] == v) {
+                    j = a;
+                    cout << "[" << m << ", " << j << "]" << endl;
+                    return 0; 
+                }
+            }
+        }
+    }
+
+    return 0;
+}
