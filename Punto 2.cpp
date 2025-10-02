305bfc0a6d69ce142c4257de7e8ec67273cd84f7
Josem-14 <josemiguelsedano9@gmail.com>
2025-10-02 00:28:50 -0500

    Update and rename Punto 2. to Punto 2.cpp

---
+++ Punto 2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string x;
+    int n = 0;
+    int i = 0;
+    int j = n - 1;
+    int palin = 1; 
+    cout << "Ingrese un número: ";
+    cin >> x;
+    
+
+    while (x[n] != '\0') {
+        n++;
+    }
+
+    while (i < j && palin == 1) {
+        if (x[i] != x[j]) {
+            palin = 0; 
+        }
+        i++;
+        j--;
+    }
+
+    if (palin == 1) {
+        cout << "El numero, " << x << " ,es palindromo." << endl;
+    } else {
+        cout << "El numero " << x << " no es palindromo." << endl;
+    }
+
+    return 0;
+}
